a55dcb364ca44b7ce7e323e6db55b0d4a18e13ba
Butnamanjunath <manjunath@zeliot.in>
2024-06-27 12:00:24 +0530

    Inventory page changes

--- src/main/java/com/sause/pages/InventoryPage.java
+++ src/main/java/com/sause/pages/InventoryPage.java
@@ -1,5 +1,11 @@
 package com.sause.pages;
 
+import java.util.List;
+
+import org.testng.Assert;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.sause.base.TestBase;
@@ -8,6 +14,34 @@ public class InventoryPage extends TestBase{
 	
 	public InventoryPage() {
 		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//div[text()='Sauce Labs Backpack']/../../following-sibling::div/button")
+	WebElement SauceLabsBackpack;
+	
+	@FindBy(xpath="//div[text()='Sauce Labs Bike Light']/../../following-sibling::div/button")
+	WebElement SauceLabsBikeLight;
+	
+	@FindBy(xpath="//div[@id='shopping_cart_container']/a/span")
+	WebElement cartItemCount;
+	
+	@FindBy(className="product_sort_container")
+	List<WebElement> sortContainer;
+	
+	public void displayAllOptions() {
+		for(WebElement e : sortContainer) {
+			System.out.println(e.getText());
+		}
+	}
+	
+	
+	public void addItemToCart() {
+		SauceLabsBackpack.click();
+//		if(cartItemCount.getText().equals(1)) {
+//			
+//		}
+		Assert.assertEquals(cartItemCount.getText(), "1");
+		
 	}
 	
 
--- src/test/java/com/sause/TestCases/InventoryPageTest.java
+++ src/test/java/com/sause/TestCases/InventoryPageTest.java
@@ -1,7 +1,44 @@
 package com.sause.TestCases;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import com.sause.base.TestBase;
+import com.sause.pages.InventoryPage;
+import com.sause.pages.LoginPage;
 
 public class InventoryPageTest extends TestBase{
+	
+	LoginPage login ;
+	InventoryPage Ip;
+	
+	public InventoryPageTest() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void setup() {
+		Intialization();
+		login = new LoginPage();
+		Ip = login.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	@Test
+	public void displayAllOptionsTest() {
+		Ip.displayAllOptions();
+		//Ip.addItemToCart();
+	}
+	
+	@Test
+	public void cartItemCount() {
+		Ip.addItemToCart();
+	}
+	
+	
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
 
 }
